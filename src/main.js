3b6277f5e90acaf34953554a6517087362b4abc5
zhaoyang <18355403288@163.com>
2019-04-25 17:42:47 +0800

    update

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker';
 import './device-init.js';
+import './style/index.less';
 
 Vue.config.productionTip = false
 
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,16 +1,79 @@
 <template>
   <div class="home">
-    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    test
+    <div class="header">
+      <p></p>
+    </div>
+    <div class="content">
+      <p class="search">搜索</p>
+      <ul>
+        <li>
+          <p>大订单</p>
+          <span>刚刚</span>
+        </li>
+      </ul>
+    </div>
+    <div class="footer">
+      <p></p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'home',
-  components: {
-    // HelloWorld
-  }
-}
+  name: "home",
+  components: {}
+};
 </script>
+
+<style lang="less" scoped>
+.home{
+  height: 100%;
+}
+.header {
+  width: 100%;
+  height: 0.6rem;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: #ccc;
+}
+
+.content{
+  height: 100%;
+  padding: 0.2rem;
+  padding-top: 0.8rem;
+  background-color: #f8f8f8;
+}
+.content .search{
+  height: 0.4rem;
+  margin-bottom: 0.3rem;
+  line-height: 0.4rem;
+  border-radius: 0.05rem;
+  text-align: center;
+  color: #ccc;
+  background-color: #fff;
+}
+.content ul li{
+  height: 1rem;
+  padding: 0.2rem;
+  margin-bottom: 0.2rem;
+  background-color: #fff;
+}
+.content ul li p{
+  margin-bottom: 0.05rem;
+  font-size: 0.24rem;
+}
+.content ul li span{
+  font-size: 0.14rem;
+}
+
+.footer {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  height: 0.6rem;
+  width: 100%;
+  background-color: #ccc;
+}
+</style>
+
