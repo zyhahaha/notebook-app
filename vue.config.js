ce15aa6870b585b1fee1cc0676e32f45a163035e
zhaoyang <18355403288@163.com>
2019-05-20 17:29:07 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  publicPath: './',
+  // assetsPublicPath: './',
   devServer: {
     port: 8888, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     proxy: {
